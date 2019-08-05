4e3c0470b475d9a3bfe2bef9e562fbfbedffb1ff
myname21-meet <myname21@meet.mit.edu>
2019-08-05 11:29:10 -0400

    done

---
+++ PersonalSite/model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+Base = declarative_base()
+class Volunteer(Base):
+	__tablename__ ="Volunteer"
+	user_id = Column(Integer,primary_key=True)
+	username = Column(String)
+	event = Column(String)
+	def __repr__(self):
+		return("volunteer_name: {}\n"
+				"Volunteer_choice: {}\n").format(
+				self.username,
+				self.event)
+			
